5b67053506f472e76d5057cb422caab5d5aef044
ChristiamRz24 <folandofler@gmail.com>
2024-05-26 14:27:09 -0500

    Comment SQL Server client and config files

--- src/databases/sqlserver/client.js
+++ src/databases/sqlserver/client.js
@@ -1,21 +1,21 @@
-import sql from "msnodesqlv8";
-import { connectionString } from "./config";
-import { processRows } from "../utils/processRows";
+// import sql from "msnodesqlv8";
+// import { connectionString } from "./config";
+// import { processRows } from "../utils/processRows";
 
-const client = {
-  query: async (query) => {
-    try {
-      const data = await new Promise((resolve, reject) => {
-        sql.query(connectionString, query, (err, rows) => {
-          if (err) reject(err);
-          else resolve(processRows(rows));
-        });
-      });
-      return data;
-    } catch (error) {
-      console.error(error);
-    }
-  },
-};
+// const client = {
+//   query: async (query) => {
+//     try {
+//       const data = await new Promise((resolve, reject) => {
+//         sql.query(connectionString, query, (err, rows) => {
+//           if (err) reject(err);
+//           else resolve(processRows(rows));
+//         });
+//       });
+//       return data;
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   },
+// };
 
-export { client };
+// export { client };
--- src/pages/api/sqlserver/tables/names.json.js
+++ src/pages/api/sqlserver/tables/names.json.js
@@ -1,16 +1,16 @@
-import { client } from "../../../../databases/sqlserver/client";
+// import { client } from "../../../../databases/sqlserver/client";
 
-export const GET = async () => {
-  const get_table_names = `
-SELECT TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG = 'mudanzas';
-  `;
+// export const GET = async () => {
+//   const get_table_names = `
+// SELECT TABLE_NAME
+// FROM INFORMATION_SCHEMA.TABLES
+// WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG = 'mudanzas';
+//   `;
 
-  try {
-    const rows = await client.query(get_table_names);
-    return new Response(JSON.stringify(rows));
-  } catch (err) {
-    return new Response(JSON.stringify(err.message));
-  }
-};
+//   try {
+//     const rows = await client.query(get_table_names);
+//     return new Response(JSON.stringify(rows));
+//   } catch (err) {
+//     return new Response(JSON.stringify(err.message));
+//   }
+// };
