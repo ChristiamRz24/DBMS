8dfab190bbc25a74db2b599486a64213429ca6e3
ChristiamRz <maholiandrade12@gmail.com>
2024-05-29 04:27:54 -0500

    Add error handling to PostgreSQL client to throw and catch errors when executing queries

--- src/databases/postgresql/client.js
+++ src/databases/postgresql/client.js
@@ -9,6 +9,7 @@ const client = {
       return rows;
     } catch (error) {
       console.error(error);
+      throw error; // Throw the error to be caught by the caller
     } finally {
       conn.release();
     }
